1d3e37ff17543e3f080582d561a764fb812c3841
Bodal-Andrey <a-v-bodal@yandex.ru>
2022-01-29 16:50:07 +0500

    Создаст стартовую структуру проекта

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './sass/style.scss';
+
+const bodyElement = document.querySelector(`body`);
+bodyElement.textContent = `Hello, hello, hello!`;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: 'development',
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  devtool: `source-map`,
+};
